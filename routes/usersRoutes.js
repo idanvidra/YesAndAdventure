3f74c197a70a88079a175aa88cdaf02f3d9232d7
idanvidra <idan.vidra@gmail.com>
2021-12-26 17:20:33 +0200

    temporarily remove token check

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -4,13 +4,17 @@ const UserCtrl = require("../controllers/users");
 const AuthHelper = require("../Helpers/authHelper");
 
 // route to get all users in the DB
-router.get("/users", AuthHelper.VerifyToken, UserCtrl.GetAllUsers);
-router.get("/user/:id", AuthHelper.VerifyToken, UserCtrl.GetUserById);
-router.get(
-    "/usernick/:nickname",
-    AuthHelper.VerifyToken,
-    UserCtrl.GetUserByNickname
-);
+// router.get("/users", AuthHelper.VerifyToken, UserCtrl.GetAllUsers);
+// router.get("/user/:id", AuthHelper.VerifyToken, UserCtrl.GetUserById);
+// router.get(
+//     "/usernick/:nickname",
+//     AuthHelper.VerifyToken,
+//     UserCtrl.GetUserByNickname
+// );
+
+router.get("/users", UserCtrl.GetAllUsers);
+router.get("/user/:id", UserCtrl.GetUserById);
+router.get("/usernick/:nickname", UserCtrl.GetUserByNickname);
 // todo: add mark and delete notification when messaging is added
 // video 155
 // router.post('/marknotification/:id', AuthHelper.VerifyToken)
