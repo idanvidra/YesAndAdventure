fc5e31272164e604d26bd37e941597fa62033014
idanvidra <idan.vidra@gmail.com>
2021-12-26 15:40:19 +0200

    added CosmosDB url to config

---
+++ config/secrets.js
@@ -0,0 +1,5 @@
+module.exports = {
+    urlForDB:
+        "mongodb://adventuretime:q5rSaGdFJMuVOrOwfc1kXwtNXWrIbFzLESqk69PO10oklIkQfC6Dhk8HyHfz4MGdoDLGpJenreTzUx0qU8r3lA%3D%3D@adventuretime.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@adventuretime@",
+    secretForAuthToken: "HitMeBabyOneMoreTime",
+};
