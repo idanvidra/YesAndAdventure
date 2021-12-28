f9c350ac02a50ba52654302b886a117bd9b71965
idanvidra <idan.vidra@gmail.com>
2021-12-28 16:44:51 +0200

    test 6

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -4,14 +4,14 @@ const router = express.Router();
 const GameCtrl = require("../controllers/games");
 const AuthHelper = require("../Helpers/authHelper");
 
-router.get("/games", AuthHelper.VerifyToken, GameCtrl.GetAllGames);
-router.get("/game/:id", AuthHelper.VerifyToken, GameCtrl.GetGame);
+// router.get("/games", AuthHelper.VerifyToken, GameCtrl.GetAllGames);
+// router.get("/game/:id", AuthHelper.VerifyToken, GameCtrl.GetGame);
 
-router.post("/game/add-game", AuthHelper.VerifyToken, GameCtrl.AddGame);
+// router.post("/game/add-game", AuthHelper.VerifyToken, GameCtrl.AddGame);
 
-// router.get("/games", GameCtrl.GetAllGames);
-// router.get("/game/:id", GameCtrl.GetGame);
+router.get("/games", GameCtrl.GetAllGames);
+router.get("/game/:id", GameCtrl.GetGame);
 
-// router.post("/game/add-game", GameCtrl.AddGame);
+router.post("/game/add-game", GameCtrl.AddGame);
 
 module.exports = router;
