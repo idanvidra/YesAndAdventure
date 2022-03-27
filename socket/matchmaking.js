acc7f855f71fe6d6efa72e299de97c692476a416
idanvidra <idan.vidra@gmail.com>
2022-03-27 16:40:14 +0300

    added feedback

--- socket/matchmaking.js
+++ socket/matchmaking.js
@@ -6,19 +6,19 @@ const Mutex = require("await-semaphore");
 module.exports = function (io, Player, _) {
     var mutex = new Mutex.Mutex();
     async function do_something(io, playerData, socket, params) {
-        socket.join(params.room);
+        // socket.join(params.room);
         playerData.EnterRoom(socket.id, params.nickname, params.room);
         // get list of nicknames of all ready to play players
         const list = playerData.GetList(params.room);
         // emit the ready event
         io.emit("playersReadyToPlay", _.uniq(list));
-        console.log("do something");
-        console.log(_.uniq(list));
+        // console.log("do something");
+        // console.log(_.uniq(list));
         if (_.uniq(list).length > 1) {
-            console.log("inside if");
-            console.log(list);
+            // console.log("inside if");
+            // console.log(list);
             const firstTwoPlayersInQueue = _.uniq(list).slice(0, 2);
-            console.log(firstTwoPlayersInQueue[0], firstTwoPlayersInQueue[1]);
+            // console.log(firstTwoPlayersInQueue[0], firstTwoPlayersInQueue[1]);
             // remove the matched players
             const player_1 = playerData.RemoveUserByNickname(
                 firstTwoPlayersInQueue[0]
@@ -26,13 +26,11 @@ module.exports = function (io, Player, _) {
             const player_2 = playerData.RemoveUserByNickname(
                 firstTwoPlayersInQueue[0]
             );
-            console.log("two players 1");
+            // console.log("two players 1");
             io.emit("matchmaking", firstTwoPlayersInQueue);
         }
         return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve("foo");
-            }, 150);
+            resolve("foo");
         });
     }
 
