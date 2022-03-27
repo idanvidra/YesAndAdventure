d9fd6167b73b063fbf621a2025e0881547230a37
idanvidra <idan.vidra@gmail.com>
2022-03-27 11:52:20 +0300

    fifth attempt - adding to player list added to critical section

--- socket/matchmaking.js
+++ socket/matchmaking.js
@@ -5,7 +5,13 @@ const Mutex = require("await-semaphore");
 
 module.exports = function (io, Player, _) {
     var mutex = new Mutex.Mutex();
-    async function do_something(list, io) {
+    async function do_something(io, playerData, socket, params) {
+        socket.join(params.room);
+        playerData.EnterRoom(socket.id, params.nickname, params.room);
+        // get list of nicknames of all ready to play players
+        const list = playerData.GetList(params.room);
+        // emit the ready event
+        io.emit("playersReadyToPlay", _.uniq(list));
         console.log("do something");
         console.log(_.uniq(list));
         if (_.uniq(list).length > 1) {
@@ -36,12 +42,12 @@ module.exports = function (io, Player, _) {
     io.on("connection", (socket) => {
         // listen for when player is ready to play
         socket.on("ready_to_play", (params) => {
-            socket.join(params.room);
-            playerData.EnterRoom(socket.id, params.nickname, params.room);
-            // get list of nicknames of all ready to play players
-            const list = playerData.GetList(params.room);
-            // emit the ready event
-            io.emit("playersReadyToPlay", _.uniq(list));
+            // socket.join(params.room);
+            // playerData.EnterRoom(socket.id, params.nickname, params.room);
+            // // get list of nicknames of all ready to play players
+            // const list = playerData.GetList(params.room);
+            // // emit the ready event
+            // io.emit("playersReadyToPlay", _.uniq(list));
             // check if there is more then one player ready
             // if (_.uniq(list).length > 1) {
             //     const firstTwoPlayersInQueue = _.uniq(list).slice(0, 2);
@@ -65,7 +71,7 @@ module.exports = function (io, Player, _) {
             // }
             // niceFetch(list);
             mutex.acquire().then((release) => {
-                do_something(list, io)
+                do_something(io, playerData, socket, params)
                     .then((res) => {
                         console.log(res);
                         release();
