4a9790e8ebe07e5c804c5f89c96781b484da9477
idanvidra <idan.vidra@gmail.com>
2022-03-27 17:56:49 +0300

    changed promise content to fit critical section

--- socket/matchmaking.js
+++ socket/matchmaking.js
@@ -7,28 +7,28 @@ const Semaphore = require("semaphore-async-await").Lock;
 module.exports = function (io, Player, _) {
     const playerData = new Player();
     async function criticalFunc(socket, playerData, params, io) {
-        socket.join(params.room);
-        playerData.EnterRoom(socket.id, params.nickname, params.room);
-        // get list of nicknames of all ready to play players
-        const list = playerData.GetList(params.room);
-        // emit the ready event
-        io.emit("playersReadyToPlay", _.uniq(list));
-        // check if there is more then one player ready
-        if (_.uniq(list).length > 1) {
-            const firstTwoPlayersInQueue = _.uniq(list).slice(0, 2);
-            // console.log(firstTwoPlayersInQueue[0], firstTwoPlayersInQueue[1]);
-            // remove the matched players
-            const player_1 = playerData.RemoveUserByNickname(
-                firstTwoPlayersInQueue[0]
-            );
-            const player_2 = playerData.RemoveUserByNickname(
-                firstTwoPlayersInQueue[0]
-            );
-            // console.log("two players 1");
-            io.emit("matchmaking", firstTwoPlayersInQueue);
-        }
         return new Promise((resolve, reject) => {
             setTimeout(() => {
+                socket.join(params.room);
+                playerData.EnterRoom(socket.id, params.nickname, params.room);
+                // get list of nicknames of all ready to play players
+                const list = playerData.GetList(params.room);
+                // emit the ready event
+                io.emit("playersReadyToPlay", _.uniq(list));
+                // check if there is more then one player ready
+                if (_.uniq(list).length > 1) {
+                    const firstTwoPlayersInQueue = _.uniq(list).slice(0, 2);
+                    // console.log(firstTwoPlayersInQueue[0], firstTwoPlayersInQueue[1]);
+                    // remove the matched players
+                    const player_1 = playerData.RemoveUserByNickname(
+                        firstTwoPlayersInQueue[0]
+                    );
+                    const player_2 = playerData.RemoveUserByNickname(
+                        firstTwoPlayersInQueue[0]
+                    );
+                    // console.log("two players 1");
+                    io.emit("matchmaking", firstTwoPlayersInQueue);
+                }
                 resolve("foo");
             }, 300);
         });
