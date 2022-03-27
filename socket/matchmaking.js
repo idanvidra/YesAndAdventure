469e97b6cb6cbb5f2edff8ed2db5848cc184612e
idanvidra <idan.vidra@gmail.com>
2022-03-27 17:32:07 +0300

    changed lock package to semaphore-async-await

--- socket/matchmaking.js
+++ socket/matchmaking.js
@@ -1,22 +1,20 @@
 // to learn more about socket.io emits:
 // https://socket.io/docs/v3/emit-cheatsheet/
 
-const Mutex = require("await-semaphore");
+const Semaphore = require("semaphore-async-await").Lock;
+// import { Lock } from "semaphore-async-await";
 
 module.exports = function (io, Player, _) {
-    var mutex = new Mutex.Mutex();
-    function do_something(io, playerData, socket, params) {
-        // socket.join(params.room);
+    const playerData = new Player();
+    async function criticalFunc(socket, playerData, params, io) {
+        socket.join(params.room);
         playerData.EnterRoom(socket.id, params.nickname, params.room);
         // get list of nicknames of all ready to play players
         const list = playerData.GetList(params.room);
         // emit the ready event
         io.emit("playersReadyToPlay", _.uniq(list));
-        // console.log("do something");
-        // console.log(_.uniq(list));
+        // check if there is more then one player ready
         if (_.uniq(list).length > 1) {
-            // console.log("inside if");
-            // console.log(list);
             const firstTwoPlayersInQueue = _.uniq(list).slice(0, 2);
             // console.log(firstTwoPlayersInQueue[0], firstTwoPlayersInQueue[1]);
             // remove the matched players
@@ -29,12 +27,12 @@ module.exports = function (io, Player, _) {
             // console.log("two players 1");
             io.emit("matchmaking", firstTwoPlayersInQueue);
         }
-        return new Promise((resolve) => {
-            resolve("foo");
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve("foo");
+            }, 300);
         });
     }
-
-    const playerData = new Player();
 
     // connection event is a global event
     io.on("connection", (socket) => {
@@ -64,21 +62,14 @@ module.exports = function (io, Player, _) {
             //     io.emit("matchmaking", firstTwoPlayersInQueue);
             // }
 
-            // function niceFetch(list) {
-            //     return mutex.use(() => do_something(list, io));
-            // }
-            // niceFetch(list);
-            mutex.acquire().then((release) => {
-                do_something(io, playerData, socket, params)
-                    .then((res) => {
-                        console.log(res);
-                        release();
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                        release();
-                    });
-            });
+            const lock = new Semaphore(1);
+
+            async function critical() {
+                await lock.acquire();
+                await criticalFunc(socket, playerData, params, io);
+                lock.release();
+            }
+            critical();
         });
 
         // listen for when ready to play player has joined game
